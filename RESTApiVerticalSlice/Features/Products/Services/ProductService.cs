071a47413bc68305107b2185e2aeaaec3b39dcd1
Nikolai <simnikola@gmail.com>
2025-12-26 20:41:16 +0300

    Minore update

--- RESTApiVerticalSlice/Features/Products/Services/ProductService.cs
+++ RESTApiVerticalSlice/Features/Products/Services/ProductService.cs
@@ -18,21 +18,18 @@ public class ProductService : IProductService
 
     public async Task<Product> CreateAsync(ProductDto dto)
     {
-        // business rules can be applied here
         var product = new Product(Guid.NewGuid(), dto.Name, dto.Price);
         return await _repo.CreateAsync(product);
     }
 
     public Task<bool> UpdateAsync(Guid id, ProductDto dto)
     {
-        // business rules, validation, events, etc.
         var product = new Product(id, dto.Name, dto.Price);
         return _repo.UpdateAsync(product);
     }
 
     public Task<bool> DeleteAsync(Guid id)
     {
-        // e.g. check references before delete
         return _repo.DeleteAsync(id);
     }
 }
